a3f6d7a65cf90a106c4b8f495de4123cd81276d4
Kishorkumar Dopathi <kishorkumardopathi16@gmail.com>
2023-04-05 00:23:46 +0530

    updated Readme.d

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # REST-With-Spring
  Spring REST with Realtime example
+
+
+![img_1.png](img/img_1.png)
+
+
+![img_2.png](img/img_2.png)
+
+![img_3.png](img/img_3.png)
+
+![img_4.png](img/img_4.png)
+
+![img.png](img/img.png)
+
+![img_5.png](img/img_5.png)
--- src/main/java/com/example/RESTWithSpring/Entity/Hospital.java
+++ src/main/java/com/example/RESTWithSpring/Entity/Hospital.java
@@ -1,9 +1,6 @@
 package com.example.RESTWithSpring.Entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
